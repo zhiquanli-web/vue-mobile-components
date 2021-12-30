ae96a5a82a13bbf9ed37a1039584db9275473e8c
lizhiquan <lizhiquan@tuzhanai.com>
2021-12-30 17:09:03 +0800

    初始化配置

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "normalize.css";
+import "@/assets/styles/index.scss";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   lintOnSave: false,
+  productionSourceMap: false,
+  devServer: {
+    open: true,
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/styles/mixin.scss";`,
+      },
+    },
+  },
 };
