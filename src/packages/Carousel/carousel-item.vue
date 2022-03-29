41dcf329931111b15b32d2508a92833bbd83df76
lizhiquan <lizhiquan@tuzhanai.com>
2022-03-29 10:30:47 +0800

    轮播组件

---
+++ src/packages/Carousel/carousel-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="carousel-item-container" role="group">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "LiCarouselItem",
+  componentName: "CarouselItem",
+  data() {
+    return {};
+  },
+  props: {
+    value: {},
+    disabled: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/packages/Carousel/carousel.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="carousel-container" role="group">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "LiCarousel",
+  componentName: "Carousel",
+  data() {
+    return {};
+  },
+  props: {
+    value: {},
+    disabled: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
